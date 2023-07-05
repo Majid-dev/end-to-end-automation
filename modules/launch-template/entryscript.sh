90a5de735643cced68e4308335d6810ee40851cd
Majid-dev <majid0karimi@gmail.com>
2023-07-06 00:54:30 +0330

    fix: import user_data commands into launch template module

--- modules/launch-template/entryscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Clone ansible playbooks from git repository
-git clone https://github.com/Majid-dev/install-jenkins.git
-cd install-jenkins
-
-#Install Jenkins and apply required configuration
-ansible-playbook install-jenkins.yml
-ansible-playbook configure-jenkins.yml
--- modules/launch-template/main.tf
+++ modules/launch-template/main.tf
@@ -3,7 +3,14 @@ resource "aws_launch_template" "template" {
   name_prefix   = "launch_template"
   image_id      = var.ami_id
   instance_type = var.instance_type
-  user_data = filebase64(var.user_data_file)
+  user_data = <<EOF
+   #!/bin/bash
+   git clone https://github.com/Majid-dev/install-jenkins.git
+   cd install-jenkins
+   ansible-playbook install-jenkins.yml
+   ansible-playbook configure-jenkins.yml
+   EOF
+
   key_name = "jenkins-key"
   network_interfaces {
     security_groups = var.security_group_ids
